9378e46731e2a0d458126ca861088d0fa07e9c4b
Clément Théraud <91315465+Cdrohm@users.noreply.github.com>
2022-01-24 15:25:48 +0100

    re upload manual

---
+++ js/api.js
@@ -0,0 +1,22 @@
+/////////////////// loading source file ///////////////////
+
+async function getPhotographers() {
+  let photographers = await fetch("../data/FishEyeData.json")
+    .then((response) => response.json())
+    .then((data) => {
+      return data.photographers;
+    });
+  return photographers;
+}
+
+async function getMedias() {
+  let medias = await fetch("../data/FishEyeData.json")
+    .then((response) => response.json())
+    .then((data) => {
+      return data.media;
+    });
+  return medias;
+}
+
+export { getPhotographers };
+export { getMedias };
---
+++ js/index.js
@@ -0,0 +1,53 @@
+//Loading content
+import { getPhotographers } from "./api.js";
+
+//initiate
+async function init() {
+  let photographers = await getPhotographers();
+  const content = document.getElementById("main-index");
+  photographers.forEach((photographer) =>
+    content.appendChild(photographerNodeFactory(photographer, photographers))
+  );
+  generateTags(photographers);
+}
+
+init();
+
+//Photograph 
+/**
+ * 
+ * @param {*} photographer 
+ * @param {*} photographers 
+ * @returns photograph card
+ */
+function photographerNodeFactory(photographer, photographers) {
+  const photographerProfile = document.createElement("article");
+  photographerProfile.id = "photographer-" + photographer.id;
+  const urlPhotographer = document.createElement("a");
+  const profilePicture = document.createElement("img");
+  const photographerName = document.createElement("h2");
+  const localisation = document.createElement("p");
+  localisation.id = "localisation";
+  const tagline = document.createElement("p");
+  tagline.id = "tagline";
+  const price = document.createElement("p");
+  price.id = "price";
+
+  urlPhotographer.href = "./html/photographer" + photographer.id + ".html"; //link to
+  profilePicture.src = "./photographersID/" + photographer.portrait; //load img
+  profilePicture.alt = photographer.name; //alt name on picture
+  photographerName.textContent = photographer.name; //photograph name
+  localisation.textContent = photographer.city + ", " + photographer.country; //localisation
+  tagline.textContent = photographer.tagline; //citation
+  price.textContent = photographer.price + "€/jour"; //price
+
+  //create
+  photographerProfile.appendChild(urlPhotographer);
+  urlPhotographer.appendChild(profilePicture);
+  urlPhotographer.appendChild(photographerName);
+  photographerProfile.appendChild(localisation);
+  photographerProfile.appendChild(tagline);
+  photographerProfile.appendChild(price);
+
+  return photographerProfile;
+}
