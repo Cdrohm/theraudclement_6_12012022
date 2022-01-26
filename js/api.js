f28d8435590f7b506063b6328260641096656c9f
Clément Théraud <theraud.clement@gmail.com>
2022-01-26 09:20:14 +0100

    identer

--- js/api.js
+++ js/api.js
@@ -1,22 +1,25 @@
-/////////////////// loading source file ///////////////////
-
+//Loading file source
 async function getPhotographers() {
   let photographers = await fetch("../data/FishEyeData.json")
-    .then((response) => response.json())
-    .then((data) => {
-      return data.photographers;
-    });
+      .then((response) => response.json())
+      .then((data) => {
+          return data.photographers;
+      });
   return photographers;
 }
 
 async function getMedias() {
   let medias = await fetch("../data/FishEyeData.json")
-    .then((response) => response.json())
-    .then((data) => {
-      return data.media;
-    });
+      .then((response) => response.json())
+      .then((data) => {
+          return data.media;
+      });
   return medias;
 }
 
-export { getPhotographers };
-export { getMedias };
+export {
+  getPhotographers
+};
+export {
+  getMedias
+};
