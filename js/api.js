f27f36f15b627207c90e1fd51c5624334c84aeef
Clément Théraud <theraud.clement@gmail.com>
2022-01-27 15:59:31 +0100

    search json error

--- js/api.js
+++ js/api.js
@@ -4,9 +4,16 @@ async function getPhotographers() {
       .then((response) => response.json())
       .then((data) => {
           return data.photographers;
-      });
+      }
+      function (rejectionReason) { // 3
+        console.log('Erreur lors de l'analyse JSON de la réponse :', rejectionReason, responseClone); // 4
+        responseClone.text() // 5
+        .then(function (bodyText) {
+            console.log('Réception des données suivantes au lieu de données JSON valides :', bodyText); // 6
+        }););
   return photographers;
 }
+
 
 async function getMedias() {
   let medias = await fetch ("../data/FishEyeData.json")
