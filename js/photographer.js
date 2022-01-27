66057230c2f35da4d47c71ab185a102129160484
Clément Théraud <theraud.clement@gmail.com>
2022-01-27 15:35:07 +0100

    Update photographer.js

--- js/photographer.js
+++ js/photographer.js
@@ -212,20 +212,6 @@ function sortBy(photographerMedias) {
   return photographerMedias;
 }
 
-//Dropdown2 menu ul/li
-/*
-function sortBy2 (photographerMedias) {
-  const option = document.getElementById("#sort-by2").value;
-
-  if (option == "popularity") {
-    photographerMedias.sort(function(a, b) {
-      return b.likes - a.likes;
-    }); 
-  }
-}
-*/
-
-
 //Form Modal
 function formModal(photographer) {
   const photographerNameContent = document.querySelector("#form-modal div.form-text");
