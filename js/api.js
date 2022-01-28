253c0813347b1152cfc8bbf54badde355a7e2d65
Clément Théraud <theraud.clement@gmail.com>
2022-01-28 10:09:52 +0100

    way git page

--- js/api.js
+++ js/api.js
@@ -1,6 +1,7 @@
 //Loading file source
 async function getPhotographers() {
   let photographers = await fetch ("../data/FishEyeData.json")
+  let photographers = await fetch ("https://github.com/Cdrohm/theraudclement_6_12012022/data/FishEyeData.json")
       .then((response) => response.json())
       .then((data) => {
           return data.photographers;
@@ -11,6 +12,7 @@ async function getPhotographers() {
 
 async function getMedias() {
   let medias = await fetch ("../data/FishEyeData.json")
+  let medias = await fetch ("https://github.com/Cdrohm/theraudclement_6_12012022/data/FishEyeData.json")
       .then((response) => response.json())
       .then((data) => {
           return data.media;
