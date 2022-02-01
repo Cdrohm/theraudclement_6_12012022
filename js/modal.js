09b7157e5539a3f8104b41d9ed66c818802a492c
Clément Théraud <theraud.clement@gmail.com>
2022-02-01 21:58:05 +0100

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -19,3 +19,20 @@ let errorMessage = document.querySelector("missmessage");
 //Regex email
 let mailRegex =
   /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+//Event launch
+launchBtn.addEventListener("click", launchModal);
+
+//Event close
+closeBtn.addEventListener("click", closeModal);
+window.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+        closeModal();
+    }
+});
+
+//Validates
+firstName.addEventListener("keyup", validFirstName);
+lastName.addEventListener("keyup", validLastName);
+mail.addEventListener("keyup", validEmail);
+message.addEventListener("keyup", validMessage);
