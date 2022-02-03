1958e51037414de12f029c4b98f1b466aef70795
Clément Théraud <theraud.clement@gmail.com>
2022-02-03 13:46:50 +0100

    Update photographer-page.js

--- js/photographer-page.js
+++ js/photographer-page.js
@@ -13,9 +13,6 @@ fetch("./data/FishEyeData.json")
     let list = new MediaList();
     let factory = new MediaFactory();
     let medias = getMedias(json.media);
-    /*let profil = json.photographers.filter(
-      (photographe) => photographe.id == query("id")
-    )[0];*/
     let photographer = new Photographer(profil);
 
     photographer.displayProfil();
