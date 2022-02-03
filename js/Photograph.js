8d18cf7ac74bf7847a21a1410b52b6bbbd282072
Clément Théraud <theraud.clement@gmail.com>
2022-02-03 09:18:05 +0100

    photo rework

--- js/Photograph.js
+++ js/Photograph.js
@@ -1,5 +1,12 @@
 //Photograph data
 class Photographer {
+    constructor(data) {
     this.name = data.name;
     this.city= data.city;
+    this.country = data.country;
+    this.tagline = data.tagline;
+    this.price = data.price;
+    this.portrait = data.portrait;
+    this.id = data.id;
+    }
 }
