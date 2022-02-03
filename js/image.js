c932f661ab8b41589acdd70518c5fce66bf90391
Clément Théraud <theraud.clement@gmail.com>
2022-02-03 13:12:12 +0100

    media js

---
+++ js/image.js
@@ -0,0 +1,37 @@
+//Object Img by mediafactory
+class Image {
+    constructor(data) {
+      this.id = data.id;
+      this.photographerId = data.photographerId;
+      this.likes = data.likes;
+      this.date = data.date;
+      this.price = data.price;
+      this.src = data.src;
+      this.title = data.title;
+      this.alt = data.alt;
+    }
+
+    //return html create
+    render() {
+      return `
+      <figure>
+       <img tabindex="0" src="medias/${this.photographerId}b/${this.src}" alt="lancer le diaporama à partir de la photo de titre ${this.title}" class="diapo" id="${this.id}"/>
+       <figcaption>
+        <h2 class="mediaTitle">${this.title}</h2>
+        <div> <span class="mediaLikes" data-id="${this.id}" >${this.likes}</span>
+        <span> <i tabindex="0" class="heart fas fa-heart" data-id="${this.id}" ></i></span>
+        </div>
+       </figcaption>
+      </figure>`;
+    }
+  
+    renderSlide() {
+      return `
+      <figure >
+       <img src="medias/${this.photographerId}b/${this.src}" alt="${this.alt}" class="diapo" id="${this.id}" />
+       <figcaption>
+        <h2 class="mediaTitle">${this.title}</h2>
+       </figcaption>
+      </figure>`;
+    }
+  }
---
+++ js/mediaFactory.js
@@ -0,0 +1,16 @@
+//MediaFactory => if img = img
+//             => if video = video (add esle if, if sound on video)
+class MediaFactory {
+    build(data) {
+      if (data.hasOwnProperty("image")) {
+        data.src = data.image;
+  
+        return new Image(data);
+      } else {
+        data.src = data.video;
+  
+        return new Video(data);
+      }
+    }
+  }
+  
